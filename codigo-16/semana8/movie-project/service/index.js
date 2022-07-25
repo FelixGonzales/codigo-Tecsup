8292da7de91d05c956a401b01dcb73b9280b26b5
FelixGonzales <alfonso.gonzalesp@gmail.com>
2022-07-25 03:25:45 -0500

    ultimos cambios

--- codigo-16/semana8/movie-project/service/index.js
+++ codigo-16/semana8/movie-project/service/index.js
@@ -14,3 +14,24 @@ export const getMovies = async () => {
         return error;
     }
 }
+
+export const storeMovieMockApi = async (movie) => {
+    try {
+        const object ={
+            title : movie.title,
+            programType : movie.programType,
+            releaseYear: movie.releaseYear,
+            imageUrl: movie.images["Poster Art"].url,
+        };
+        const response = await fetch(mockAPIUrl, {
+            method: "POST",
+            body: JSON.stringify(object),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return response;
+    }catch (error){
+        return error;
+    }
+}
